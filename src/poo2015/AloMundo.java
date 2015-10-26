5247d772a66d5e638075294db5096abf8b1872d4
antssousa <antssousa@gmail.com>
2015-10-26 17:04:20 -0200

    Primeiro envio para o github, código referente ao Alô Mundo

---
+++ src/poo2015/AloMundo.java
@@ -0,0 +1,11 @@
+package poo2015;
+
+public class AloMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Alô Mundo, estou no git hub!");
+
+	}
+
+}
