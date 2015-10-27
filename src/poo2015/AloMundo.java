c48b1ffed8adec679d1cb78a53a1b2c98ea49fa3
Lázaro <lazarovasconceloscoutinho@gmail.com>
2015-10-27 09:34:20 -0300

    Adicionando código do alô mundo.

--- src/poo2015/AloMundo.java
+++ src/poo2015/AloMundo.java
@@ -7,6 +7,7 @@ public class AloMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "Alô mundo, estou aprendo git e github, criando branches");
+		JOptionPane.showMessageDialog(null, "Alô mundo Lázaro!!!");
 
 	}
 
