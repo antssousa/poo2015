5015908c938d2e4f719a3a30d6317a527bee34c6
jhonpatrick <patrickrodrigueszik52@gmail.com>
2015-10-29 17:48:25 -0200

    Ta lascado mais uma vez

--- src/poo2015/AloMundo.java
+++ src/poo2015/AloMundo.java
@@ -12,7 +12,9 @@ public class AloMundo {
 		JOptionPane.showMessageDialog(null, "(n + 1) % 61");
 		int i = 0;
 		while(i < 100){
+			JOptionPane.showMessageDialog(null, "Alterações feitas por: Patrick Rodrigues");
 			JOptionPane.showMessageDialog(null, " Acabei de roubar a senha da sua conta do banco! :p");
+			
 			i++;
 		}
 	}
