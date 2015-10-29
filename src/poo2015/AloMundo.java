2231090b9713757ebab6d38e9355789edcf26ba0
aluno-parnaiba <nohopeleft13@live.com>
2015-10-29 17:47:23 -0200

    Batata

--- src/poo2015/AloMundo.java
+++ src/poo2015/AloMundo.java
@@ -6,10 +6,8 @@ public class AloMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "Alô mundo, eu sou Danny!!!");
+		JOptionPane.showMessageDialog(null, "Batata is very coll");
 		
-		
-		JOptionPane.showMessageDialog(null, "(n + 1) % 61");
 	}
 
 }
