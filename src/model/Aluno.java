b56f6b66f99c62d062b405b7464f171d663cbfe4
jhonpatrick <patrickrodrigueszik52@gamil.com>
2015-10-29 17:02:34 -0200

    Adicionando classe aluno

---
+++ src/model/Aluno.java
@@ -0,0 +1,67 @@
+package model;
+
+public class Aluno {
+	
+	private String nome;
+	private int idade;
+	
+	public Aluno() {
+		super();
+	}
+
+	public Aluno(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + "]";
+	}
+
+}
